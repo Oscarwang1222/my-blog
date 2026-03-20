76457722acbd7c201d709b1bbe28a6a877182854
Oscarwang1222 <527691098@qq.com>
2026-03-20 19:38:40 +0800

    Fix: Blog comment path detection for baseUrl compatibility

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -2,11 +2,13 @@ import BlogPostItem from '@theme-original/BlogPostItem';
 import type {Props} from '@theme/BlogPostItem';
 import Comment from '@site/src/components/Comment';
 import {useLocation} from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import React from 'react';
 
 export default function BlogPostItemWrapper(props: Props): React.JSX.Element {
   const location = useLocation();
-  const isBlogPostPage = location.pathname.startsWith('/blog/') && 
+  const blogPath = useBaseUrl('/blog/');
+  const isBlogPostPage = location.pathname.startsWith(blogPath) && 
                           !location.pathname.endsWith('/blog') &&
                           !location.pathname.includes('/tags/') &&
                           !location.pathname.includes('/authors/');
